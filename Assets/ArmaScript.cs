ba89b35b08b76708d8a56040a4b7afb3a6acc0c7
alessandrorico99 <alessandrorico99@gmail.com>
2022-01-02 19:38:50 +0100

    Indentato

--- Assets/ArmaScript.cs
+++ Assets/ArmaScript.cs
@@ -4,71 +4,85 @@ using UnityEngine;
 
 public class ArmaScript : MonoBehaviour
 {
-    public bool FireType= true ;
-    public float damage= 10f ;
-    public float range = 100f ;
+    public int Magazine = 30;
+    public bool FireType = true;
+    public float damage = 10f;
+    public float range = 100f;
 
-    public float FireRate =15f;    
+    public float FireRate = 15f;
     public Camera fpsCam;
 
-    public GameObject Impact ; 
+    public GameObject Impact;
 
-    
-    public ParticleSystem muzzleFlash ;
 
-    private float RateoFuoco = 0f ;
+    public ParticleSystem muzzleFlash;
+
+    private float RateoFuoco = 0f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B) && FireType == true) 
+        if (Input.GetKeyDown(KeyCode.B) && FireType == true)
         {
-            FireType = false ; 
-        } 
-
-        else if (Input.GetKeyDown(KeyCode.B) && FireType == false) 
-        {
-            FireType = true ; 
-        } 
-
-        if (FireType == false)
-        {
-            if (Input.GetButton("Fire1")&& Time.time >= RateoFuoco)  
-            {
-                RateoFuoco = Time.time + 1f / FireRate ; //Messo il fuoco rapido 
-                Shoot();
-                Debug.Log(FireType);
-            }
+            FireType = false;
         }
-        if (FireType == true) 
+
+        else if (Input.GetKeyDown(KeyCode.B) && FireType == false)
         {
-            if(Input.GetButtonDown("Fire1"))
+            FireType = true;
+        }
+
+        if (Magazine != 0)
+        {
+            if (FireType == false)
             {
-                Shoot();  
-                 Debug.Log(FireType);          //FuocoSingolo 
+                if (Input.GetButton("Fire1") && Time.time >= RateoFuoco && Magazine != 0)
+                {
+                    RateoFuoco = Time.time + 1f / FireRate; //Messo il fuoco rapido 
+                    Shoot();
+                }
+            }
+            if (FireType == true)
+            {
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    Shoot();
+                    //FuocoSingolo 
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Reload();
             }
         }
     }
 
     void Shoot()
-    {       
-        
-            muzzleFlash.Play();
-            RaycastHit hit;
+    {
 
-            if (Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward,out hit,range)) 
+        Magazine -= 1;
+        muzzleFlash.Play();
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
             {
-                Debug.Log(hit.transform.name);
-
-               Target target = hit.transform.GetComponent<Target>();
-
-               if (target!=null) 
-               {
-                   target.takedmg(damage) ;
-               }
-
-               GameObject ImpatctGun= Instantiate(Impact,hit.point,Quaternion.LookRotation(hit.normal)) ;
-
-               Destroy(ImpatctGun,1f) ;
+                target.takedmg(damage);
             }
+
+            GameObject ImpatctGun = Instantiate(Impact, hit.point, Quaternion.LookRotation(hit.normal));
+
+            Destroy(ImpatctGun, 1f);
+        }
+    }
+
+    void Reload()
+    {
+        int StateMagazine = Magazine;
+        Magazine = Magazine + (30 - StateMagazine);
     }
 }
